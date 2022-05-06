765c0d1538875a6d593518caac32c442fea9b7a8
Ashley Gibson <ashley@nosegraze.com>
2022-05-06 13:54:29 +0100

    Refactor & rename main plugin class.

--- naked-social-share.php
+++ naked-social-share.php
@@ -32,166 +32,39 @@
  */
 
 // Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+use Ashleyfae\NakedSocialShare\Plugin;
+
+if (! defined('ABSPATH')) {
+    exit;
 }
 
-if ( ! class_exists( 'Naked_Social_Share' ) ) :
+if (version_compare(phpversion(), '7.4', '<')) {
+    return;
+}
 
-	class Naked_Social_Share {
+// Plugin version.
+const NSS_VERSION = '1.5.2';
 
-		/**
-		 * The single instance of the plugin.
-		 * @var Naked_Social_Share
-		 * @since 1.0.0
-		 */
-		private static $instance = null;
+// Plugin Folder Path.
+define('NSS_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
-		/**
-		 * Naked_Social_Share instance.
-		 *
-		 * Insures that only one instance of Naked_Social_Share exists at any one time.
-		 *
-		 * @uses   Naked_Social_Share::setup_constants() Set up the plugin constants.
-		 * @uses   Naked_Social_Share::includes() Include any required files.
-		 * @uses   Naked_Social_Share::load_textdomain() Load the language files.
-		 *
-		 * @access public
-		 * @since  1.0.0
-		 * @return Naked_Social_Share Instance of Naked_Social_Share class
-		 */
-		public static function instance() {
+// Plugin Folder URL.
+define('NSS_PLUGIN_URL', plugin_dir_url(__FILE__));
 
-			if ( ! isset( self::$instance ) && ! self::$instance instanceof Naked_Social_Share ) {
-				self::$instance = new Naked_Social_Share;
-				self::$instance->setup_constants();
+// Plugin Root File.
+const NSS_PLUGIN_FILE = __FILE__;
 
-				add_action( 'plugins_loaded', array( self::$instance, 'load_textdomain' ) );
-
-				self::$instance->includes();
-			}
-
-			return self::$instance;
-
-		}
-
-		/**
-		 * Throw error on object clone.
-		 *
-		 * The whole idea of the singleton design pattern is that there is a single
-		 * object therefore, we don't want the object to be cloned.
-		 *
-		 * @access protected
-		 * @since  1.0.0
-		 * @return void
-		 */
-		public function __clone() {
-			// Cloning instances of the class is forbidden.
-			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'naked-social-share' ), '1.0.0' );
-		}
-
-		/**
-		 * Disable unserializing of the class.
-		 *
-		 * @access protected
-		 * @since  1.0.0
-		 * @return void
-		 */
-		public function __wakeup() {
-			// Unserializing instances of the class is forbidden.
-			_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'naked-social-share' ), '1.0.0' );
-		}
-
-		/**
-		 * Setup plugin constants.
-		 *
-		 * @access private
-		 * @since  1.0.0
-		 * @return void
-		 */
-		private function setup_constants() {
-
-			// Plugin version.
-			if ( ! defined( 'NSS_VERSION' ) ) {
-				define( 'NSS_VERSION', '1.5.2' );
-			}
-
-			// Plugin Folder Path.
-			if ( ! defined( 'NSS_PLUGIN_DIR' ) ) {
-				define( 'NSS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-			}
-
-			// Plugin Folder URL.
-			if ( ! defined( 'NSS_PLUGIN_URL' ) ) {
-				define( 'NSS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-			}
-
-			// Plugin Root File.
-			if ( ! defined( 'NSS_PLUGIN_FILE' ) ) {
-				define( 'NSS_PLUGIN_FILE', __FILE__ );
-			}
-
-		}
-
-		/**
-		 * Include Required Files
-		 *
-		 * @access private
-		 * @since  1.0.0
-		 * @return void
-		 */
-		private function includes() {
-
-			global $nss_options;
-
-			// Settings.
-			require_once NSS_PLUGIN_DIR . 'includes/admin/settings/register-settings.php';
-			if ( empty( $nss_options ) ) {
-				$nss_options = nss_get_settings();
-			}
-
-			require_once NSS_PLUGIN_DIR . 'includes/class-naked-social-share-buttons.php';
-			require_once NSS_PLUGIN_DIR . 'includes/functions.php';
-
-			if ( is_admin() ) {
-				require_once NSS_PLUGIN_DIR . 'includes/admin/admin-pages.php';
-				require_once NSS_PLUGIN_DIR . 'includes/admin/upgrades.php';
-				require_once NSS_PLUGIN_DIR . 'includes/admin/settings/display-settings.php';
-			}
-
-			require_once NSS_PLUGIN_DIR . 'includes/install.php';
-
-		}
-
-		/**
-		 * Loads the plugin language files.
-		 *
-		 * @access public
-		 * @since  1.0.0
-		 * @return void
-		 */
-		public function load_textdomain() {
-
-			$lang_dir = dirname( plugin_basename( NSS_PLUGIN_FILE ) ) . '/lang/';
-			$lang_dir = apply_filters( 'naked-social-share/languages-directory', $lang_dir );
-			load_plugin_textdomain( 'naked-social-share', false, $lang_dir );
-
-		}
-
-	}
-
-endif;
+require_once __DIR__.'/vendor/autoload.php';
 
 /**
  * Loads the whole plugin.
  *
  * @since 1.0.0
- * @return Naked_Social_Share
+ * @return Plugin
  */
-function Naked_Social_Share() {
-	$instance = Naked_Social_Share::instance();
-
-	return $instance;
+function Naked_Social_Share()
+{
+    return Plugin::instance();
 }
 
-Naked_Social_Share();
+Naked_Social_Share()->boot();
---
+++ src/Plugin.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Plugin.php
+ *
+ * @package   naked-social-share
+ * @copyright Copyright (c) 2022, Ashley Gibson
+ * @license   GPL2
+ */
+
+namespace Ashleyfae\NakedSocialShare;
+
+/**
+ * @since 2.0 Renamed from `Naked_Social_Share`
+ */
+class Plugin
+{
+    /**
+     * Single instance of the plugin.
+     *
+     * @var Plugin|null
+     * @since 1.0.0
+     */
+    protected static ?Plugin $instance = null;
+
+    /**
+     * Retrieves the single instance of this class.
+     *
+     * @since  1.0.0
+     * @return static
+     */
+    public static function instance(): Plugin
+    {
+        if (! isset(static::$instance) && ! static::$instance instanceof Plugin) {
+            static::$instance = new static;
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * Throw error on object clone.
+     *
+     * The whole idea of the singleton design pattern is that there is a single
+     * object therefore, we don't want the object to be cloned.
+     *
+     * @since  1.0.0
+     * @return void
+     */
+    public function __clone()
+    {
+        // Cloning instances of the class is forbidden.
+    }
+
+    /**
+     * Disable unserializing of the class.
+     *
+     * @since  1.0.0
+     * @return void
+     */
+    public function __wakeup()
+    {
+        // Unserializing instances of the class is forbidden.
+    }
+
+    /**
+     * Boots the plugin.
+     *
+     * @since 2.0.0
+     */
+    public function boot(): void
+    {
+        $this->includes();
+
+        // Backwards compatibility for renamed classes.
+        class_alias(Plugin::class, 'Naked_Social_Share');
+
+        add_action('plugins_loaded', [static::$instance, 'load_textdomain']);
+    }
+
+    /**
+     * Include Required Files
+     *
+     * @since  1.0.0
+     * @return void
+     */
+    private function includes()
+    {
+        global $nss_options;
+
+        // Settings.
+        require_once NSS_PLUGIN_DIR.'includes/admin/settings/register-settings.php';
+        if (empty($nss_options)) {
+            $nss_options = nss_get_settings();
+        }
+
+        require_once NSS_PLUGIN_DIR.'includes/class-naked-social-share-buttons.php';
+        require_once NSS_PLUGIN_DIR.'includes/functions.php';
+
+        if (is_admin()) {
+            require_once NSS_PLUGIN_DIR.'includes/admin/admin-pages.php';
+            require_once NSS_PLUGIN_DIR.'includes/admin/upgrades.php';
+            require_once NSS_PLUGIN_DIR.'includes/admin/settings/display-settings.php';
+        }
+
+        require_once NSS_PLUGIN_DIR.'includes/install.php';
+    }
+
+    /**
+     * Loads the plugin language files.
+     *
+     * @since  1.0.0
+     * @return void
+     */
+    public function load_textdomain()
+    {
+        $lang_dir = dirname(plugin_basename(NSS_PLUGIN_FILE)).'/lang/';
+        $lang_dir = apply_filters('naked-social-share/languages-directory', $lang_dir);
+        load_plugin_textdomain('naked-social-share', false, $lang_dir);
+    }
+}
