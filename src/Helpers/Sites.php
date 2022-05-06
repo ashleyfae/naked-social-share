89f1f262bb799fe1b334e5229243d768e0f9242e
Ashley Gibson <ashley@nosegraze.com>
2022-05-06 14:35:02 +0100

    Start building out site helper.

---
+++ src/Helpers/Sites.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Sites.php
+ *
+ * @package   naked-social-share
+ * @copyright Copyright (c) 2022, Ashley Gibson
+ * @license   MIT
+ */
+
+namespace Ashleyfae\NakedSocialShare\Helpers;
+
+class Sites
+{
+
+}
